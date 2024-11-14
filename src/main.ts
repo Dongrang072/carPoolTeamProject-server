6c0985669d93f1658bf4001e1d6360107e375cb4
Dongrang072 <robot4097@gmail.com>
2024-11-14 13:51:24 +0900

    feat console log in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log("dasdadsa");
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
